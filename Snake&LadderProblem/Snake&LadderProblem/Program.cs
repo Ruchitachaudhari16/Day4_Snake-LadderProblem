7b85a30beaaf941c0fe69c250efaa351a87965b2
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-18 16:10:59 -0400

    [Ruchita] Add: Start position Player

---
+++ Snake&LadderProblem/Snake&LadderProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_LadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake & Ladder Problem");
+            Simulation simulation = new Simulation();
+            simulation.StartPosition();
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Snake&LadderProblem/Snake&LadderProblem/Simulation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_LadderProblem
+{
+     public  class Simulation
+    {
+        //Uc1:-Snake and Ladder game played with single player at start position 0
+
+        public void StartPosition()
+        {
+             int startPosition = 0;
+            Console.WriteLine("Game played with single player at StartPosition:" + startPosition);
+        }
+    }
+}
