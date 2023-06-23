9eee92fd5d7884a0aac9bea8abc77a2589d96a9f
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-22 20:15:21 -0400

    [Ruchita] Add: Wiining position

--- Snake&LadderProblem/Snake&LadderProblem/Simulation.cs
+++ Snake&LadderProblem/Snake&LadderProblem/Simulation.cs
@@ -10,49 +10,52 @@ namespace Snake_LadderProblem
 {
     public class Simulation
     {
-﻿
-            public int startPositiont = 0;
-            public int RolledDie = 0;
-            public int Choice = 0;
-            int Option = 0;
-            public void StartPlaying()
-            {
-                Console.WriteLine("Game started");
-                while (startPositiont != 100)
-                {
-                    Random random = new Random();
-                RolledDie = random.Next(1, 7);  //Random value betw 1 to 6
-                    Console.WriteLine("Dice Number is : " + RolledDie);
-                    Choice = random.Next(0, 3); //value to check its snake or ladder
-                    Console.WriteLine("Dice value :" + Choice);
-                 
 
-                    switch (Choice)
-                    {
-                        case 0:  //for no play if choice =0
-                            Console.WriteLine("No Play");
-                            break;
-                        case 1:                         //if choice =1
-                            startPositiont += RolledDie;    //startPoisition = startPoisition +RolledDie
-                        Console.WriteLine("Got Ladder: " + startPositiont);
-                            break;
-                        case 2:
-                            startPositiont -= RolledDie;        //if choice=2
-                            Console.WriteLine("snake Attack: " + startPositiont); //startPoisition -RolledDie
+        public int startPositiont = 0;
+        public int RolledDie = 0;
+        public int Choice = 0;
+        int Option = 0;
+        public void StartPlaying()
+        {
+            //Ensure the player gets to exact winningposition 100. - Note in case the player position
+            Console.WriteLine("Game started");
+            while (startPositiont != 100)
+            {
+                Random random = new Random();
+                RolledDie = random.Next(1, 7);  //Random value betw 1 to 6
+                Console.WriteLine("Dice Number is : " + RolledDie);
+                Choice = random.Next(0, 3); //value to check its snake or ladder
+                Console.WriteLine("Dice value :" + Choice);
+
+                switch (Choice)
+                {
+                    case 0:  //for no play if choice =0
+                        Console.WriteLine("No Play");
                         break;
-                        default:
-                            Console.WriteLine("Invalid Option");
-                            break;
-                    }
-                    if (startPositiont < 0) //if 0<0 then it will start again
-                    {
-                        startPositiont = 0;
-                    }
+                    case 1:                         //if choice =1
+                        startPositiont += RolledDie;  //startPoisition = startPoisition +randDice
+                        if (startPositiont > 100)
+                        {
+                            startPositiont -= startPositiont; //
+                        }
+                        Console.WriteLine("Exact Position: " + startPositiont);
+                        break;
+                    case 2:
+                        startPositiont -= RolledDie;        //if choice=2
+                        if (startPositiont < 0)  
+                        {
+                            startPositiont = 0;
+                        }
+                        Console.WriteLine("snake Attack: " + startPositiont); 
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
                 }
             }
         }
     }
-
+}
 
 
 
